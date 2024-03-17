a309bb452227f2be0ab10f7522e2b25bdbef22f5
sohila <sohilayasser24@gmail.com>
2024-03-17 13:07:08 +0200

    edit newMessage path

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -3,33 +3,32 @@ const User = require("../model/User");
 const Message = require("../model/Message");
 
 const getMessages = async (req, res, next) => {
-    const {chat_,page_} = req.query
+  const { chat_, page_ } = req.query;
 
-    // console.log({chat_,page_});
-    try {
-        if(chat_&&page_){
-            
-            const messages = await Message.find({chat:chat_},"-__v").sort({createdAt:"desc"})
-            .populate("sender","_id email firstname lastname")
-            .skip(page_*10).limit(10);
+  // console.log({chat_,page_});
+  try {
+    if (chat_ && page_) {
+      const messages = await Message.find({ chat: chat_ }, "-__v")
+        .sort({ createdAt: "desc" })
+        .populate("sender", "_id email firstname lastname")
+        .skip(page_ * 10)
+        .limit(10);
 
-            return res.status(200).json({
-                messages
-            })
-        }
-
-        return res.status(400).json({
-            chat_Query:"Is required",
-            page_Query:"Is required"
-        })
-
-    
-    } catch (error) {
-        res.status(500).json({
-            message: "Process Failed",
-            error: error.message,
-          });
+      return res.status(200).json({
+        messages,
+      });
     }
+
+    return res.status(400).json({
+      chat_Query: "Is required",
+      page_Query: "Is required",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Process Failed",
+      error: error.message,
+    });
+  }
 };
 
 const setMessages = async (req, res, next) => {
@@ -42,11 +41,11 @@ const setMessages = async (req, res, next) => {
       chat: chatId,
     });
 
-    newMessage = await newMessage.populate("sender");
-    newMessage = await newMessage.populate("chat");
+    newMessage = await Message.populate("sender");
+    newMessage = await Message.populate("chat");
     newMessage = await User.populate(newMessage, {
-      path: "chat.users",
-      select: "name email",
+      path: "chat.members",
+      select: "name",
     });
 
     await Chat.findByIdAndUpdate(req.body.chatId, {
