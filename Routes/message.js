272fb130fee05f5f85c5f196b2835a6abc8656b7
sohila <sohilayasser24@gmail.com>
2024-03-20 22:18:05 +0200

    add delete and edit message

--- Routes/message.js
+++ Routes/message.js
@@ -1,9 +1,16 @@
 const express = require("express");
-const { setMessages, getMessages } = require("../Controllers/message");
+const {
+  setMessages,
+  getMessages,
+  deleteMessage,
+  editMessage,
+} = require("../Controllers/message");
 const { protect } = require("../Middlewares/auth");
 const router = express.Router();
 
 router.post("/", protect, setMessages);
 router.get("/", protect, getMessages);
+router.delete("/delete/:messageId", protect, deleteMessage);
+router.put("/edit/:messageId", protect, editMessage);
 
 module.exports = router;
