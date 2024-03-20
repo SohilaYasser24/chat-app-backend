e57158d84c471535ea8499614ad398be245bf334
sohila <sohilayasser24@gmail.com>
2024-03-21 01:22:06 +0200

    edit private chat route

--- Routes/chat.js
+++ Routes/chat.js
@@ -14,6 +14,6 @@ const { protect } = require("../Middlewares/auth");
 router.get("/", protect, getChats);
 router.get("/fetchAllGroups", protect, getGroups);
 router.post("/", protect, createChat);
-router.post("/:recieverId", protect, createPrivateChat);
+router.post("/privateChat/:recieverId", protect, createPrivateChat);
 
 module.exports = router;
